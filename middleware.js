dd9fdfad346847c6226be2f0989c41745c9df52f
Dev Gupta <guptadev265@gmail.com>
2022-10-12 17:27:50 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
 export async function middleware(req) {
-  if (req.nextUrl.pathname === "/") {
+  if (req.nextUrl.pathname === "/UserDashBoard" && "/" && "/DoctorDashBoard") {
     const session = await getToken({
       req,
       secret: process.env.JWT_SECRET,
