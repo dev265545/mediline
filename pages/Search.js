7d9dca75c7fff41076e2ebda78e0e73f9a48fded
Dev Gupta <guptadev265@gmail.com>
2022-10-15 01:50:35 +0530

    Update Search.js
    Fix empty query and adding preventDefault

--- pages/Search.js
+++ pages/Search.js
@@ -13,10 +13,16 @@ function Search() {
     });
   }, []);
   console.log(listdoctors);
-  const HandleSearch =()=>{
+  const HandleSearch =(e)=>{
+    e.preventDefault()
+    if(Query===""){
+      axios.get(`http://localhost:3000/api/doctors/alldoctors`).then((resp) => {
+        setlistdoctors(resp.data.data);
+      });
+    }else{
     axios.get(`http://localhost:3000/api/doctors/searchdoctors?query=${Query}`).then((resp) => {
       setlistdoctors(resp.data.data);
-    });
+    });}
     setQuery("")
   }
   return (
@@ -117,10 +123,10 @@ function Search() {
                   class=" p-4 w-96 z-50 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
              dark:focus:border-blue-500"
                   placeholder="Search Mockups, Logos, Design Templates..."
-                  required
+                 
                 />
                 <button
-                onClick={()=>{HandleSearch()}}
+                onClick={(e)=>{HandleSearch(e)}}
                   type="submit"
                   class="absolute top-0 right-0 p-4 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
