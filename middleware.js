88310254e9470a46288803f17762d8d4dd317e6e
SilverMINDX01 <gupta.archit01@gmail.com>
2022-10-18 12:41:45 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -8,7 +8,7 @@ export async function middleware(req) {
       secret: process.env.JWT_SECRET,
       secureCookie: process.env.NODE_ENV === "production",
     });
-    // You could also check for any property on the session object,
+    // You could also for any property on the session object,
     // like role === "admin" or name === "John Doe", etc.
     if (!session) return NextResponse.redirect("http://localhost:3000/login");
     // If user is authenticated, continue.
