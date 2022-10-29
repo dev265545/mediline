d841b417322e2967584f3d2f45a1353ef4d8bdd7
Dev Gupta <guptadev265@gmail.com>
2022-10-29 10:24:22 +0530

    Update Search.js

--- pages/Search.js
+++ pages/Search.js
@@ -122,7 +122,7 @@ function Search() {
                   id="search-dropdown"
                   className=" p-4 w-96 z-50 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
              dark:focus:border-blue-500"
-                  placeholder="Search Mockups, Logos, Design Templates..."
+                  placeholder="Search by name,location,address,city,specialization ....."
                  
                 />
                 <button
