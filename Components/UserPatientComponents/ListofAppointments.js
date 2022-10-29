57056ddba99fd57cfbe34cd7cc74d48c2ca51668
Dev Gupta <guptadev265@gmail.com>
2022-10-29 11:59:10 +0530

    Update ListofAppointments.js

--- Components/UserPatientComponents/ListofAppointments.js
+++ Components/UserPatientComponents/ListofAppointments.js
@@ -92,7 +92,7 @@ console.log(c)
                 </div>
                 <div className="flex-1 min-w-0">
                   <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-                    Dr. {doctor?.name}
+                     {doctor?.name}
                   </p>
                   <p className="text-sm text-gray-500 truncate dark:text-gray-400">
                     {doctor?.email}
